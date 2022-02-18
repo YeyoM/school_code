046e233de1904dc130b09c20dbdacedb22568521
YeyoM <yeyomoreno2003@hotmail.com>
2022-02-18 08:36:08 -0600

    saturday project

--- semester_02/Leng_Comp/Ejercicios_Tarea/OOP.cpp
+++ semester_02/Leng_Comp/Ejercicios_Tarea/OOP.cpp
@@ -44,15 +44,20 @@ class Cuadrado:public Figura {
   private:
     string nombre;
     float area;
+    float perimetro;
   public:
     Cuadrado(float, string);
-    void calcularArea();
+    ~Cuadrado();
     void mostrar();
+    void calcularArea();
+    void calcularPerimetro();
 };
 
 Cuadrado::Cuadrado(float _lado, string _nombre):Figura(_lado) {
   nombre = _nombre;
 }
+
+Cuadrado::~Cuadrado() {}
 
 void Cuadrado::mostrar() {
   Figura::mostrar();
@@ -64,6 +69,11 @@ void Cuadrado::calcularArea() {
   area = lado1 * lado1;
   cout << "Area: " << area << "cm2 "<< endl;
 }
+
+void Cuadrado::calcularPerimetro() {
+  perimetro = lado1 * 4;
+  cout << "Perimetro: " << perimetro << "cm "<< endl;
+}
 ///////////////////////////////////////////////////////
 // Clase Hija (Rectangulo)
 class Rectangulo:public Figura {
@@ -71,16 +81,21 @@ class Rectangulo:public Figura {
     string nombre;
     float lado2;
     float area;
+    float perimetro;
   public:
     Rectangulo(float, float, string);
-    void calcularArea();
+    ~Rectangulo();
     void mostrar();
+    void calcularArea();
+    void calcularPerimetro();
 };
 
 Rectangulo::Rectangulo(float _lado1, float _lado2, string _nombre):Figura(_lado1) {
   lado2 = _lado2;
   nombre = _nombre;
 }
+
+Rectangulo::~Rectangulo() {}
 
 void Rectangulo::mostrar() {
   Figura::mostrar();
@@ -94,12 +109,90 @@ void Rectangulo::calcularArea() {
   cout << "Area: " << area << "cm2 "<< endl;
 }
 
+void Rectangulo::calcularPerimetro() {
+  perimetro = (lado1 * 2) + (lado2 * 2);
+  cout << "Perimetro: " << perimetro << "cm "<< endl;
+}
+
 ///////////////////////////////////////////////////////
 // Clase Hija (Triangulo)
+class Triangulo:public Figura {
+  private:
+    string nombre;
+    float lado2;
+    float lado3;
+    float lado4;
+    float area;
+    float perimetro;
+  public:
+    Triangulo(float, float, float, float, string);
+    ~Triangulo();
+    void mostrar();
+    void calcularArea();
+    void calcularPerimetro();
+};
 
+Triangulo::Triangulo(float _lado1, float _lado2, float _lado3, float _lado4, string _nombre):Figura(_lado1) {
+  lado2 = _lado2;
+  lado3 = _lado3;
+  lado4 = _lado4;
+  nombre = _nombre;
+}
+
+Triangulo::~Triangulo() {}
+
+void Triangulo::mostrar() {
+  Figura::mostrar();
+  cout << nombre << endl;
+  cout << "Longitud de la base: " << lado1 << "cm" << endl;
+  cout << "Longitud de la altura: " << lado2 << "cm" << endl;
+}
+
+void Triangulo::calcularArea() {
+  area = (lado1 * lado2) / 2;
+  cout << "Area: " << area << "cm2 "<< endl;
+}
+
+void Triangulo::calcularPerimetro() {
+  perimetro = lado1 + lado3 + lado4;
+  cout << "Perimetro: " << perimetro << "cm "<< endl;
+}
 ///////////////////////////////////////////////////////
-// Clase Hija (Circulo)
+// Clase Circulo
+class Circulo {
+	private:
+    string nombre;
+		float radio;
+    float area;
+    float perimetro;
+	public:
+		Circulo(float, string);
+		~Circulo();
+    void mostrar();
+		void calcularArea();
+    void calcularPerimetro();
+};
 
+Circulo::Circulo(float _radio, string _nombre) {
+	radio = _radio;
+  nombre = _nombre;
+}
+
+Circulo::~Circulo() {}
+
+void Circulo::mostrar() {
+  cout << "Figura: " << nombre << endl;
+}
+
+void Circulo::calcularArea() {
+	area = ((3.1416 * radio) * radio);
+	cout << "Area: " << area << "cm2" << endl;
+}
+
+void Circulo::calcularPerimetro() {
+	perimetro = ((2 * 3.1416) * radio);
+	cout << "Perimetro: " << perimetro << "cm" << endl;
+}
 ///////////////////////////////////////////////////////
 
 
@@ -112,15 +205,21 @@ int main () {
   int opcion;
 
   // variables para la inicializacion de objetos
-  float longitud_lado1, longitud_lado2;
+  float longitud_lado1,   // bases
+        longitud_lado2,   // alturas
+        longitud_lado3,   // lados restantes para calculo 
+        longitud_lado4;   // de perimetro de triangulo
 
   do {
 
+    system("cls");
+    cout << endl;
+
     cout << "Bienvenido" << endl;
-    cout << "1) Calcular area de un cuadrado" << endl;
-    cout << "2) Calcular area de un rectangulo" << endl; 
-    cout << "3) Calcular area de un triangulo" << endl; 
-    cout << "4) Calcular area de un circulo" << endl; 
+    cout << "1) Calcular area y perimetro de un cuadrado" << endl;
+    cout << "2) Calcular area y perimetro de un rectangulo" << endl; 
+    cout << "3) Calcular area y perimetro de un triangulo" << endl; 
+    cout << "4) Calcular area y perimetro de un circulo" << endl; 
     cout << "5) Salir" << endl; 
 
     cout << "Ingrese una opcion valida: ";
@@ -136,6 +235,10 @@ int main () {
       Cuadrado cuadrado(longitud_lado1, "Cuadrado");
       cuadrado.mostrar();
       cuadrado.calcularArea();
+      cuadrado.calcularPerimetro();
+
+      cout << endl;
+      system("pause");
 
     } else if (opcion == 2) {
       // rectangulo
@@ -150,12 +253,50 @@ int main () {
       Rectangulo rectangulo(longitud_lado1, longitud_lado2, "Rectangulo");
       rectangulo.mostrar();
       rectangulo.calcularArea();
+      rectangulo.calcularPerimetro();
+
+      cout << endl;
+      system("pause");
 
     } else if (opcion == 3) {
       // triangulo
+      cout << "Ingrese la longitud de la base: ";
+      cin >> longitud_lado1;
+      cout << endl;
+
+      cout << "Ingrese la longitud de la altura: ";
+      cin >> longitud_lado2;
+      cout << endl;
+
+      cout << "Ingrese la longitud de los lados restantes para calcular el perimetro" << endl;
+      cout << "Primer lado: ";
+      cin >> longitud_lado3;
+      cout << endl;
+      cout << "Segundo lado: ";
+      cin >> longitud_lado4;
+      cout << endl;
+
+      Triangulo triangulo(longitud_lado1, longitud_lado2, longitud_lado3, longitud_lado4, "Triangulo");
+      triangulo.mostrar();
+      triangulo.calcularArea();
+      triangulo.calcularPerimetro();
+
+      cout << endl;
+      system("pause");
 
     } else if (opcion == 4) {
       // circulo
+      cout << "Ingrese la longitud del radio del circulo: ";
+      cin >> longitud_lado1;
+      cout << endl;
+
+      Circulo circulo(longitud_lado1, "Circulo");
+      circulo.mostrar();
+      circulo.calcularArea();
+      circulo.calcularPerimetro();
+
+      cout << endl;
+      system("pause");
       
     } else if (opcion == 5) {
       // salir
@@ -164,6 +305,8 @@ int main () {
     } else {
       // default
       cout << "Digite una opcion valida" << endl;
+      cout << endl;
+      system("pause");
     }
 
     cout << endl;
@@ -171,6 +314,8 @@ int main () {
     longitud_lado2 = 0;
 
   } while( salir == false );
+
+  system("pause");
 
   return 0;
 }
