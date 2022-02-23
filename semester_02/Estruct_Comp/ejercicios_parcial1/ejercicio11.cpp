8395e6217613aca72435550a2019762f39a5f204
YeyoM <yeyomoreno2003@hotmail.com>
2022-02-23 17:27:21 -0600

    plantilla para ejercicio11

--- semester_02/Estruct_Comp/ejercicios_parcial1/ejercicio11.cpp
+++ semester_02/Estruct_Comp/ejercicios_parcial1/ejercicio11.cpp
@@ -2,6 +2,16 @@
   Realizar 2 problemas que ustedes investiguen por su cuenta, donde utilicen para su solución
   el uso de memoria dinámica. (11-12)
 
-  Imaginemos el siguiente caso:
-  De un API, se obtiene un array de
+  1) Dados 3 arreglos desarrolla un algoritmo para juntarlo en uno usando
+     memoria dinamica
+  2) Dado un arreglo de numeros, descarta los negativos y solo deja a los
+     positivos
 */
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+  return 0;
+}
--- semester_02/torneo/problem5.cpp
+++ semester_02/torneo/problem5.cpp
@@ -1,5 +1,38 @@
 #include <iostream>
 #include <iomanip>
+
+using namespace std;
+
+int main(){
+    int n;
+    string Nombre;
+    float m = 0, m1 = 0, tS = 0, tS1 = 0;
+    float z = 0, z1 = 0, tB = 0, tB1 = 0;
+    float l = 0, l1 = 0, tA = 0, tA1 = 0;
+
+    cin>>n;
+
+    for (int x = 0; x < n; x++){
+
+        cin>>Nombre;
+
+        cin>> m >> z >> l;
+        do{
+            cin>> m1 >> z1 >> l1;
+        } while(m1 >= z && z1 >= z && l1 >= l);
+        
+        tS1 += m1; tB1 += z1; tA1 += l1;
+        tS += m; tB += z; tA += l;
+    }
+
+   cout<<"Puntos de saque: "<<fixed<<setprecision(2)<<(tS1 / tS)*100<<" %."<<endl;
+   cout<<"Puntos de bloqueo: "<<fixed<<setprecision(2)<<(tB1 / tB)*100<<" %."<<endl;
+   cout<<"Puntos de ataque: "<<fixed<<setprecision(2)<<(tA1 / tA)*100<<" %."<<endl;
+}
+
+
+/*#include <iostream>
+#include <iomanip>
 using namespace std;
 int main()
 {
@@ -9,21 +42,15 @@ int main()
   float l[100], l1[100], l2[100];
   char nombre[20];
   float puntos_saque = 0, puntos_bloqueo = 0, puntos_ataque = 0;
-  cout << fixed;
-  cout << setprecision(2);
   cin >> n_players;
   for (int i = 0; i < n_players; i++)
   {
     fflush(stdin);
-    cin.getline(nombre, 30, '\n');
+    cin.getline(nombre,30,'\n');
     fflush(stdin);
-    scanf("%)
     cin >> n[i];
     cin >> n1[i];
     cin >> n2[i];
-
-
-
     cin >> l[i];
     cin >> l1[i];
     cin >> l2[i];
@@ -34,11 +61,11 @@ int main()
     lau1 += l1[i];
     lau2 += l2[i];
   }
-  puntos_saque = (lau * 100) / aux;
-  puntos_bloqueo = (lau1 * 100) / aux1;
-  puntos_ataque = (lau2 * 100) / aux2;
-
-  cout << "Puntos de saque: " << puntos_saque << " %." << endl;
-  cout << "Puntos de bloqueo: " << puntos_bloqueo << " %." << endl;
-  cout << "Puntos de ataque: " << puntos_ataque << " %." << endl;
-}
+  // puntos_saque = (lau * 100) / aux;
+  // puntos_bloqueo = (lau1 * 100) / aux1;
+  // puntos_ataque = (lau2 * 100) / aux2;
+  system("cls");
+  cout<<"Puntos de saque: "<<fixed<<setprecision(2)<< (lau / aux) * 100<<" %.";
+   cout<<"Puntos de bloqueo: "<<fixed<<setprecision(2)<<(lau1 / aux1)*100<<" %.";
+   cout<<"Puntos de ataque: "<<fixed<<setprecision(2)<<(lau2 / aux2) * 100<<" %.";
+}*/
