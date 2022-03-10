d97eed44b3e1b620fba4790cd7ff36c75cf5a7b4
YeyoM <yeyomoreno2003@hotmail.com>
2022-03-10 07:53:59 -0600

    insert new nodes

--- semester_02/Estruct_Comp/Ejercicios_Clase/lista_doblemente_enlazada.cpp
+++ semester_02/Estruct_Comp/Ejercicios_Clase/lista_doblemente_enlazada.cpp
@@ -43,7 +43,10 @@ void insertarNodo(Nodo *&lista, int n) {
   nuevo_nodo -> dato = n;
 
   Nodo *aux1 = lista;
-  Nodo *aux2;
+  Nodo *aux2 = NULL;  // este siempre va anerior a aux1
+  Nodo *aux3 = NULL;  // este lo usaremos para que cuando insertemos
+                      // un nodo, el que le sigue apunte a ese nuevo nodo
+                      // para cuando sea el primer elemento
 
   // recorremos la lista mientras aux1 (iterador) sea diferente de null
   // y que el doto del nodo iterador sea menor al dato, esto nos deja
@@ -54,21 +57,39 @@ void insertarNodo(Nodo *&lista, int n) {
   }
 
   if(lista == aux1){
-    lista = nuevo_nodo;
-    lista -> anterior = NULL;
-    // if(lista -> siguiente != NULL) {
-    //   aux2 = lista -> siguiente;
-    //   aux2 -> anterior = lista;
-    // }
+    // este procedimiento para cuando se inserte al inicio de la lista
+    lista = nuevo_nodo;             // lista apunta al nuevo nodo    
+    if(lista -> siguiente != 0) {   // si no es el unico elemento
+      aux3 = lista -> siguiente;    // llamamos a una variable auxiliar que 
+      aux3 -> anterior = lista;     // guarda a la posicion siguiente, para 
+    }                               // que ese siguiente apunte al primero
   } else {
-    aux2 -> siguiente = nuevo_nodo; 
-    nuevo_nodo -> anterior = aux2;
+    // en caso contrario que sea en medio o al final
+    aux2 -> siguiente = nuevo_nodo; // el nodo anterior al que vamos a ingresar apunta en siguiente al nuevo
+    nuevo_nodo -> anterior = aux2;  // el nuevo nodo, su anterior apunta al aux2, el nodo menor que el
   }
 
-  nuevo_nodo -> siguiente = aux1;
+  nuevo_nodo -> siguiente = aux1; // el nuevo nodo apunta en siguiente a aux1, el nodo mayor que el
+  
+  // en caso de que el que insertemos no sea el ultimo elemento
+  if(aux1 != 0) {
+    aux1 -> anterior = nuevo_nodo;  // ese elemento mayor al nuevo nodo
+  }                                 // su anterior sera el nuevo nodo
+
 
   cout << "Elemento " << n << " ingresado correctamente" << endl;
 
+
+  // Pruebasssssss
+  // Nodo *anterior_nuevo = nuevo_nodo -> anterior;
+  // Nodo *siguiente_nuevo = nuevo_nodo -> siguiente;
+  // 
+  // if(anterior_nuevo != 0) {
+  //   cout << "Anterior: " <<anterior_nuevo -> dato << endl;\
+  // }
+  // if(siguiente_nuevo != 0) {
+  //   cout << "Siguiente: " << siguiente_nuevo -> dato << endl;
+  // }
 }
 
 void mostrarLista(Nodo *lista) {
@@ -83,6 +104,8 @@ void mostrarLista(Nodo *lista) {
     actual = actual -> siguiente;     // cambiamos al siguiente nodo
   }
   cout << actual -> dato << endl;
+
+  cout << "cambio" << endl;
 
   // nos detenemos ahi para que el nodo acutual no
   // sea null y poder recorrer de atras para delante
