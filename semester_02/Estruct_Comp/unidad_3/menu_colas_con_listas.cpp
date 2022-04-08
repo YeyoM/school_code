6fdb70ec6f3c9305f8f8aa2aa86d773036af4a4a
YeyoM <yeyomoreno2003@hotmail.com>
2022-04-08 11:52:58 -0500

    menu_colas_listas

--- semester_02/Estruct_Comp/unidad_3/menu_colas_con_listas.cpp
+++ semester_02/Estruct_Comp/unidad_3/menu_colas_con_listas.cpp
@@ -1,14 +1,31 @@
-#include<iostream>
+// Cesar Donet Hernandez Gastelum
+// Diego Emilio Moreno Sanchez @YeyoM
+// 8 de abril de 2022
+
+#include <iostream>
 using namespace std;
 
+// ESTRUCTURA DE LOS NODOS DE LA PILA ////////////////
 struct Nodo {
   int dato;
   Nodo *siguiente;
 };
 
-void cola_circular(int n){
+// PROTOTIPOS DE LAS FUNCIONES ////////////////////////
+void buscarDato(Nodo *lista, int dato);
+void insertarDato(Nodo *&start, Nodo *&end, int dato);
+void eliminarDato(Nodo *&start, Nodo *&end);
 
-  /////////////////////////////////////
+
+int main(){
+
+  // AQUI DEFINIMOS LA LONGITUD DE LA COLA ////////////
+  int tam_cola;
+  cout << "Digite el tamaño de la cola: ";
+  cin >> tam_cola;
+    
+  // AQUI DEFINIMOS ESTRUCTURAS PARA LLEVAR EL CONTROL 
+  // DE LA COLA
   Nodo *start;
   start = NULL;
   Nodo *end;
@@ -19,76 +36,65 @@ void cola_circular(int n){
 
   int dato;
   int opcion;
-  /////////////////////////////////////
 
+
+  // MOSTRAMOS EL MENU ////////////////////////////////
   do{
-      cout << "1. Insertar" << endl;
-      cout << "2. Eliminar" << endl;
-      cout << "3. Mostrar"  << endl;
-      cout << "4. Salir"    << endl;
-      cout << "Digite una opcion: ";
-      cin >> opcion;
-      switch(opcion){
-          case 1:
-              if(fin==n){
-                  cout<<"Cola llena"<<endl;
-              }else{
-                  cout<<"Digite un numero: ";
-                  cin>>dato;
-                  cola[fin]=dato;
-                  fin++;
-              }
-              break;
-          case 2:
-              if(frente==fin){
-                  cout<<"Cola vacia"<<endl;
-              }else{
-                  cout<<"El dato eliminado es: "<<cola[frente]<<endl;
-                  frente++;
-              }
-              break;
-          case 3:
-              if(frente==fin){
-                  cout<<"Cola vacia"<<endl;
-              }else{
-                  for(i=frente;i<fin;i++){
-                      cout<<cola[i]<<" ";
-                  }
-                  cout<<endl;
-              }
-              break;
-          case 4:
-              break;
-          default:
-              cout<<"Opcion no valida"<<endl;
-      }
-  }while(opcion!=4);
-}
-
-int main(){
-    int n;
-    cout<<"Digite el tamaño de la cola: ";
-    cin>>n;
-    cola_circular(n);
+    cout << "1. Insertar"         << endl;
+    cout << "2. Mostrar/Eliminar" << endl;
+    cout << "3. Buscar"           << endl;
+    cout << "4. Salir"            << endl;
+    cout << "Digite una opcion: ";
+    cin  >> opcion;
+    switch(opcion){
+      // CASO 1 PARA INSERTAR UN ELEMENTO A LA COLA ///
+        if(fin == tam_cola){
+          cout << "Cola llena" << endl;
+        } else{
+          cout << "Digite un numero: ";
+          cin >> dato;
+          insertarDato(start, end, dato);
+          fin++;
+        }
+        break;
+      // CASO 2 PARA ELIMINAR UN ELEMENTO A LA COLA ///
+      case 2:
+        if(inicio == fin){
+          cout << "Cola vacia" << endl;
+        } else{
+          eliminarDato(start, end);
+          inicio++;
+        }
+        break;
+      // CASO 3 PARA BUSCAR UN ELEMENTO A LA COLA /////
+      case 3:
+        if(inicio == fin){
+          cout << "Cola vacia" << endl;
+        } else{
+          cout << "Digite un numero: ";
+          cin >> dato;
+          buscarDato(start, dato);
+          cout << endl;
+        }
+        break;
+      /////////////////////////////////
+      case 4:
+        break;
+      /////////////////////////////////
+      default:
+        cout << "Opcion no valida" << endl;
+    }
+  } while(opcion != 4);
     return 0;
 }
 
-
-/*
-Nodo *start;
-start = NULL;
-Nodo *end;
-end = NULL;
-
-int inicio = -1;
-int fin = -1;
-
-void buscarDato(Nodo *lista, int n) {
+///////////////////////////////////////
+void buscarDato(Nodo *pila, int dato) {
   bool encontrado = false;
   Nodo *actual = new Nodo();
-  actual = lista;
+  actual = pila;
   while(actual != NULL) {
-    if (actual -> dato == n){ 
+    if (actual -> dato == dato){ 
       encontrado = true;
       cout << "Encontrado!!!" << endl;
       break;
@@ -100,6 +106,7 @@ void buscarDato(Nodo *lista, int n) {
   }
 }
 
+///////////////////////////////////////
 void insertarDato(Nodo *&start, Nodo *&end, int dato) {
   Nodo *nuevo_nodo = new Nodo;
   nuevo_nodo -> dato = dato;
@@ -112,8 +119,10 @@ void insertarDato(Nodo *&start, Nodo *&end, int dato) {
     end -> siguiente = nuevo_nodo;
     end = nuevo_nodo;
   }
+  cout << "El numero: " << nuevo_nodo -> dato << " ha sido insertado" << endl;
 }
 
+///////////////////////////////////////
 void eliminarDato(Nodo *&start, Nodo *&end) {
   if (start == NULL) {
     cout << "La cola esta vacia" << endl;
@@ -124,5 +133,3 @@ void eliminarDato(Nodo *&start, Nodo *&end) {
     delete aux;
   }
 }
-
-*/
