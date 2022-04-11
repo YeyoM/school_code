ab98d097a2f512dceff67cb25d20b4fd7e043401
YeyoM <yeyomoreno2003@hotmail.com>
2022-04-11 11:08:06 -0500

    algoritmo para convertir de octal a dec

---
+++ semester_02/Estruct_Comp/Ejercicios_Clase/octal_a_decimal.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int octal(int n, int i);
+
+int main(){
+  int k = octal(114, 0);
+  printf("En octal es %d",k);
+  return 0;
+}
+int octal(int n, int i){
+  int coc, residuo, res;
+  coc = n/8;
+  residuo = n % 8;
+  if (coc != 0){
+    res = residuo * pow(10, i) + octal(coc, i + 1);
+  }
+  else {
+    res = residuo * pow(10, i);
+  }
+  return res;
+}
