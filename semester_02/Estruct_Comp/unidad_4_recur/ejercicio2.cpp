63818916aad54a00ce70982766b4def1fd5b508b
YeyoM <yeyomoreno2003@hotmail.com>
2022-04-24 18:22:22 -0500

    ejercicio 2 unidad 4

---
+++ semester_02/Estruct_Comp/unidad_4_recur/ejercicio2.cpp
@@ -0,0 +1,24 @@
+// Función recursiva que recibe un número entero y retorna la sumatoria 
+// de los dígitos que lo forman. Ejemplo: n=34219, la función regresa 19
+
+#include <iostream>
+using namespace std;
+
+// Write a recursive function that returns the sum of the digits in a number
+// Ejemplo: n=34219, la función regresa 19
+int sum_digits(int n) {
+  if (n < 10) {
+    return n;
+  } else {
+    return n % 10 + sum_digits(n / 10);
+  }
+}
+
+int numero;
+
+int main() {
+  cout << "Ingresa un numero: ";
+  cin >> numero;
+  cout << "La suma de los digitos del numero " << numero << " es: " << sum_digits(numero);
+  return 0;
+}
