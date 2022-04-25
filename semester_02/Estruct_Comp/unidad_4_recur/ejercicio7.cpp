8c176ec48616765a58526aec45908157bb5788bc
YeyoM <yeyomoreno2003@hotmail.com>
2022-04-24 20:39:21 -0500

    ejercicio 7 unidad 4

---
+++ semester_02/Estruct_Comp/unidad_4_recur/ejercicio7.cpp
@@ -0,0 +1,30 @@
+// Se presenta la solución recursiva a encontrar el numero más 
+// grande de un vector, implementar la lógica planteada en este 
+// código en lenguaje C
+
+#include <iostream>
+using namespace std;
+
+int mayor_num(int *vector, int iterador, int mayor) {
+  if (iterador == 0) {
+    return mayor;
+  }
+  else if (vector[iterador] > mayor) {
+    return mayor_num(vector, iterador - 1, vector[iterador]);
+  } 
+  else {
+    return mayor_num(vector, iterador - 1, mayor);
+  }
+}
+
+int vector [5] = { 1, 30, 2, 5, 2 }; 
+int iterador = 5;
+
+int main() {
+  cout << "El vector es: " << endl;
+  for (int i = 0; i < 5; i++) {
+    cout << vector[i] << endl;
+  }
+  cout << "El numero mayor es: " << mayor_num(vector, iterador, vector[iterador]);
+  return 0;
+}
