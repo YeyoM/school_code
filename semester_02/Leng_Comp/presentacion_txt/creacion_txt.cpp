cdfd240cbafa21583815c8698066b26d7ad6c766
YeyoM <yeyomoreno2003@hotmail.com>
2022-05-24 10:40:12 -0500

    formatting code for presentation

--- semester_02/Leng_Comp/presentacion_txt/creacion_txt.cpp
+++ semester_02/Leng_Comp/presentacion_txt/creacion_txt.cpp
@@ -7,46 +7,45 @@ using namespace std;
 //Funcion para escribir en un blog de notas
 //Forma sencilla
 void escribir() {
-    ofstream archivo; // ofstream es para la escritura
-    string nombreArchivo, frase;
+  // ofstream es para la escritura
+  ofstream archivo; 
+  string nombreArchivo, frase;
+  cout << "Digite el nombre del archivo (termine con .txt): ";
+  getline(cin, nombreArchivo);
 
-    cout << "Digite el nombre del archivo (termine con .txt): ";
-    getline(cin, nombreArchivo);
-
-    archivo.open(nombreArchivo.c_str(), ios::out); // ios::out si existe el archivo lo remplaza o si no existe lo crea
-
-    if (archivo.fail()) {   //.fail() fallo, manda un true o false, true = existe un error
-        cout << "No se pudo abrir el archivo";
-        exit(1); // sirve para salir del programa en caso de falla
-    }
-    cout << endl << "Digite el texto del archivo: ";
-    getline(cin, frase);
-    archivo << frase;   // tiene la misma funcion que cout y el cin
-    archivo.close(); //cerrar el archivo
+  // ios::out si existe el archivo lo remplaza o si no existe lo crea
+  archivo.open(nombreArchivo.c_str(), ios::out); 
+  //.fail() fallo, manda un true o false, true = existe un error
+  if (archivo.fail()) {   
+    cout << "No se pudo abrir el archivo";
+    exit(1); // sirve para salir del programa en caso de falla
+  }
+  cout << endl << "Digite el texto del archivo: ";
+  getline(cin, frase);
+  archivo << frase;   // tiene la misma funcion que cout y el cin
+  archivo.close(); //cerrar el archivo
 }
 
 //lectura de archivos
 void lectura() {
-    string texto, nombreArchivo;
-    ifstream archivo; // ifstream es para la lectura
-    cout << "Que archivo desea abrir: "; getline(cin, nombreArchivo);
-    archivo.open(nombreArchivo.c_str(), ios::in);
-    if (archivo.fail()) {
-        cout << "No se pudo abrir el archivo";
-        exit(1);
-    }
-    while (!archivo.eof()) { //eof recorre todo el archivo
-        getline(archivo, texto);
-        cout << texto << endl;
-    }
-    archivo.close();
+  string texto, nombreArchivo;
+  ifstream archivo; // ifstream es para la lectura
+  cout << "Que archivo desea abrir: "; getline(cin, nombreArchivo);
+  archivo.open(nombreArchivo.c_str(), ios::in);
+  if (archivo.fail()) {
+    cout << "No se pudo abrir el archivo";
+    exit(1);
+  }
+  while (!archivo.eof()) { //eof recorre todo el archivo
+    getline(archivo, texto);
+    cout << texto << endl;
+  }
+  archivo.close();
 }
 
-
-int main()
-{
-    escribir();
-    lectura();
-    system("pause");
-    return 0;
+int main() {
+  escribir();
+  lectura();
+  system("pause");
+  return 0;
 }
--- semester_02/Leng_Comp/presentacion_txt/ejercicion_palindromos.cpp
+++ semester_02/Leng_Comp/presentacion_txt/ejercicion_palindromos.cpp
@@ -6,60 +6,67 @@
 using namespace std;
 
 string texto;
- int  igual = 0, x = 0;
- //edita la copia de la variable texto y la revierte
+int igual = 0, x = 0;
+
+//edita la copia de la variable texto y la revierte
 void reverseStr(string& str) {
-    int n = str.length();
- 
-    for (int i = 0; i < n / 2; i++)
-        swap(str[i], str[n - i - 1]);
+  int n = str.length();
+
+  for (int i = 0; i < n / 2; i++)
+    swap(str[i], str[n - i - 1]);
 }
- //comparacion de cada palabra.
-void evaluar(string texto){
+
+//comparacion de cada palabra.
+void evaluar(string texto) {
 	string reverse;
-	reverse=texto;
+  reverse = texto;
 	reverseStr(reverse);
     for(int i = 0; i < texto.length(); i++) {
-        if(texto[i] == reverse[i]) {//evalua si cada letra es igual, la variable texto que es la que leyo del archivo y la variable reverse que es la copia de texto pero invertida
-            igual++;
-        }
+      // evalua si cada letra es igual, la variable texto que 
+      // es la que leyo del archivo y la variable reverse que 
+      // es la copia de texto pero invertida
+      if(texto[i] == reverse[i]) {
+        igual++;
+      }
     }
-    if(texto.length() == igual) { //si el contador de las veces que fue igual es indentico al tama�o de letras de la palabra entonces si es palindroma
-        x= 1;
+    // si el contador de las veces que fue igual es 
+    // indentico al tama�o de letras de la palabra 
+    // entonces si es palindroma
+    if(texto.length() == igual) { 
+      x = 1;
     } else {
-    x= 0;
+    x = 0;
 	}
-	igual=0;  //reinciamos el contador;
- }
+	igual = 0;  //reinciamos el contador;
+}
 
 //lectura de archivos
 void lectura() {
-	
-    string  nombreArchivo;
-    ifstream archivo; // ifstream es para la lectura
-    cout << "Que archivo desea abrir: (escriba el nombre del archivo seguido con .txt)"; getline(cin, nombreArchivo);
-    archivo.open(nombreArchivo.c_str(), ios::in);
-    if (archivo.fail()) {
-        cout << "No se pudo abrir el archivo\n";
-        exit(1);
-    }
-    cout<<"\t\tEl contenido del archivo txt es este:\n\n";
-    while (!archivo.eof()) { //eof recorre todo el archivo
-		getline(archivo, texto);
-	    evaluar(texto);
-	    
-        if(x==1){
-        	cout<<texto<<" es palindroma\n";
-		}else{
-			cout<<texto<<" no es palindroma\n";
-		}
-		x=0;
-	
-    }
-    archivo.close();
+  string nombreArchivo;
+  ifstream archivo; // ifstream es para la lectura
+  cout << "Que archivo desea abrir: (escriba el nombre del archivo seguido con .txt)"; 
+  getline(cin, nombreArchivo);
+  archivo.open(nombreArchivo.c_str(), ios::in);
+  if (archivo.fail()) {
+      cout << "No se pudo abrir el archivo\n";
+      exit(1);
+  }
+  cout<< "\t\tEl contenido del archivo txt es este:\n\n";
+  while (!archivo.eof()) { //eof recorre todo el archivo
+	  getline(archivo, texto);
+	  evaluar(texto);
+	  
+    if(x == 1) {
+      cout << texto << " es palindroma\n";
+	  } else {
+	  	cout << texto << " no es palindroma\n";
+	  }
+	  x = 0;
+  }
+  archivo.close();
 }
  
 int main() {
-   lectura();
-    return 0;
+  lectura();
+  return 0;
 }
