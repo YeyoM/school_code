71277ec9f1b63ea210223b425508a1b4a6b6a1df
YeyoM <yeyomoreno2003@hotmail.com>
2022-06-15 17:45:08 -0500

    busqueda binaria

---
+++ semester_02/Estruct_Comp/algoritmos_unidad_final/binarySearch.cpp
@@ -0,0 +1,31 @@
+//Librerias
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+bool busquedaBinaria(int arr[], int tam, int valor) {
+  int inicio = 0;
+  int fin = tam - 1;
+  int medio;
+  while (inicio <= fin) {
+    medio = (inicio + fin) / 2;
+    if (arr[medio] == valor) {
+      return true;
+    } else if (arr[medio] < valor) {
+      inicio = medio + 1;
+    } else {
+      fin = medio - 1;
+    }
+  }
+  return false;
+}
+
+//Busqueda binaria
+int main() {
+  int arr[] = {5, 2, 4, 6, 1, 3};
+  int n = sizeof(arr) / sizeof(arr[0]);
+  bool encontrado = busquedaBinaria(arr, n, 5);
+  cout << encontrado << endl;
+  return 0;
+}
