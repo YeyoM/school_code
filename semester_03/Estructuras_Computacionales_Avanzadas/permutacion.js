485660c4f4fa8d5b5ca14c2ee79750719a373a08
YeyoM <yeyomoreno2003@hotmail.com>
2022-08-12 08:32:00 -0500

    java

--- semester_03/Estructuras_Computacionales_Avanzadas/permutacion.js
+++ semester_03/Estructuras_Computacionales_Avanzadas/permutacion.js
@@ -26,19 +26,6 @@ const seleccionarUno = (arr, seleccionados, iterador) => {
 
 const mostrarPermutaciones = (arr) => {
   var result = [];
-    var backtrack = (i, nums) => {
-      if(i===nums.length){
-        result.push(nums.slice());
-        return;
-      }  
-      for(let j = i; j < nums.length; j++){
-        [nums[i],nums[j]] = [nums[j],nums[i]];
-        backtrack(i+1, nums);
-        [nums[i],nums[j]] = [nums[j],nums[i]];
-      }
-    }
-    backtrack(0, nums);
-    console.log(result);
     return result;
 }
 
