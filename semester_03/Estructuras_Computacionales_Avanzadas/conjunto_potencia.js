2ad3e2fc953ca440c6005815058b4e2625724e01
YeyoM <yeyomoreno2003@hotmail.com>
2022-08-16 09:21:43 -0500

    conjunto potencia

---
+++ semester_03/Estructuras_Computacionales_Avanzadas/conjunto_potencia.js
@@ -0,0 +1,60 @@
+// Diego Emilio Moreno Sanchez
+// Lenguaje: javascript
+// Path: conjunto_potencia.js
+
+// Definicion de los valores de entrada
+const arr = ['a', 'b', 'c', 'd']  // -> conjunto de elementos
+const n = arr.length              // -> numero de elementos del conjunto
+
+// Elimina los elementos duplicados de un array de arrays
+const eliminarDuplicados = (arr) => {
+
+  let ordenado = arr.map(arr => arr.sort())     // -> ordenamos los arrays, de tal modo que los que 
+                                                //    tienen los mismos elementos sean iguales
+  let stringArr = ordenado.map(JSON.stringify)  // -> convertir el array a string  
+  let setUnicoArray = new Set(stringArr)        // -> convertir el string a un set
+                                                // ya que un set no permite elementos duplicados
+                                                // y los elimina automaticamente
+  let sinDuplicadosArray = Array.from(
+    setUnicoArray, 
+    JSON.parse
+  )                                             // -> convertir el set a un array sin duplicados  
+
+  return sinDuplicadosArray                     // -> retornar el array sin duplicados          
+}
+
+const conjuntoPotencia = (inputArr) => {
+  let conjuntoPotenciaParcial = []  
+  let conjuntoPotencia = []
+
+  // Para cada arreglo en el array, crear un nuevo arreglo que es una copia del original,
+  // pero con el elemento actual removido, y luego llamar a la funcion recursiva sobre ese 
+  // nuevo arreglo, mientras tambien agregar el elemento actual a la lista de permutaciones.
+  const combinar = (arr, m = []) => {
+    conjuntoPotenciaParcial.push(m)
+    conjuntoPotenciaParcial.push(arr)
+    for (let i = 0; i < arr.length; i++) {
+      let actual = arr.slice()                      // -> copia del arreglo original
+      let siguiente = actual.splice(i, 1)           // -> remover el elemento actual del arreglo
+      combinar(actual.slice(), m.concat(siguiente)) // -> llamar a la funcion recursiva sobre el nuevo arreglo
+    }
+  }
+
+  combinar(inputArr)
+  
+  // Eliminamos los elementos duplicados de la lista de permutaciones parciales
+  conjuntoPotencia = eliminarDuplicados(conjuntoPotenciaParcial)
+
+  return conjuntoPotencia
+}
+
+// Funcion que calcula el numero de subconjuntos de un conjunto de elementos
+const subconjuntos = (n) => {
+  return Math.pow(2, n)
+}
+
+const result = conjuntoPotencia(arr)
+const resultSubConjuntos = subconjuntos(n)
+
+console.log(result)
+console.log(resultSubConjuntos)
