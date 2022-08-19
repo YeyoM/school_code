6077bedd3c15269bf482f7488217c301a50c7b64
YeyoM <yeyomoreno2003@hotmail.com>
2022-08-19 09:42:29 -0500

    programa nube java

---
+++ semester_03/Lenguajes de Computacion 3/nube/nube.java
@@ -0,0 +1,11 @@
+public class nube {
+  int weight;
+  int width;
+  public nube(int w, int wi) {
+      weight = w;
+      width = wi;
+  }
+  public int rain() {
+      return weight * width;
+  }
+}
---
+++ semester_03/Lenguajes de Computacion 3/nube/principal.java
@@ -0,0 +1,16 @@
+public class principal {
+  public static void main(String[] args)
+  {
+      nube nube1=new nube(10, 20);
+      nube nube2= new nube(12,21);
+
+      if(nube1.rain()>nube2.rain())
+      {
+          System.out.println("nube 1 es mayor");
+      }
+      if(nube1.rain()<nube2.rain())
+      {
+          System.out.println("nube 2 es mayor");
+      }
+  }
+}
