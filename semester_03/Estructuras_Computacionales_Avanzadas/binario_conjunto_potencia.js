38136d519eb78514f1a6c01804fd01e0173ed7df
YeyoM <yeyomoreno2003@hotmail.com>
2022-08-22 07:59:33 -0500

    tabla binaria con conjunto potencia

--- semester_03/Estructuras_Computacionales_Avanzadas/binario_conjunto_potencia.js
+++ semester_03/Estructuras_Computacionales_Avanzadas/binario_conjunto_potencia.js
@@ -26,19 +26,31 @@ const numPosibleCombinaciones = (length) => {
 }
 
 const tablaBinaria = (arr, length) => {
+  // Sacamos el numero de posibles combinaciones, para luego sacar el numero
+  // en binario del numero posible de combinaciones y poder llenar la tabla
+  // binaria correctamente y despues poder calcular el conjunto potencia
   let numPosiblesCombinaciones = numPosibleCombinaciones(length) - 1
   let maxLengthBinary = decimalBinario(numPosiblesCombinaciones).split('').length
+  // Creamos un loop que cicle de 0 al numero de posibles combinaciones
+  // que es el numero se subconjuntos dentro del conjunto potencia
   for (let i = 0; i <= numPosiblesCombinaciones; i++) {
+    // Generamos el binario de i con todos los ceros posibles
     let binary = decimalBinario(i, maxLengthBinary).split('').reverse()
     let result = []
+    // recorremos digito por digito el numero binario recien creado
     for (let j = 0; j < binary.length; j++) {
+      // si el digito actual es 1 agregamos el elemento del array original
+      // al nuevo array de resultados
       if (binary[j] === '1') {
         result.push(arr[j])
       }
     }
-    console.log(`${binary.reverse().join('')} = ${result}`)
+    // Una vez recorridos todos los digitos del numero binario de i
+    // Podeos imprimir el binario y el conjunto potencia que le corresponda
+    console.log(`${binary.reverse().join(' ')} -> {${result}}`)
   }
 }
 
-console.log(array)
-tablaBinaria(array, length)
+console.log(array.reverse().join(' '))
+console.log('------------------------')
+tablaBinaria(array.reverse(), length)
