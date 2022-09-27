178100ea32e33f8f9d0762411b18db2a93827c06
YeyoM <yeyomoreno2003@hotmail.com>
2022-09-26 23:05:46 -0500

    componentes conexas ejercicio finished :)

--- semester_03/Estructuras_Computacionales_Avanzadas/grafos/componentes_conexas.js
+++ semester_03/Estructuras_Computacionales_Avanzadas/grafos/componentes_conexas.js
@@ -74,7 +74,7 @@ const getNodesOrder = (nodesCount) => {
     nodesOrder.push(index)
     nodesCount[index] = -1
   }
-  return nodesOrder
+    return nodesOrder
 }
 
 const sortMatrixOnes = (matrix) => {
@@ -101,7 +101,88 @@ const sortMatrixColumns = (matrix, nodesOrder) => {
   return matrix
 }
 
-const getComponentesConexas = (matrix) => {
+const getUnderDiagonalNodes = (matrix) => {
+  let coordinates = []
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (matrix[i][j] === 1) {
+        coordinates.push([i, j])
+      }
+    }
+  }
+  return coordinates
+}
+
+// check if there are other coordinates with the i or j value
+// if there are, then they are part of the same component
+// if not, then they are part of different components
+// function here...
+const divideComponents = (coordinates) => {
+  let components = []
+  for (let i = 0; i < coordinates.length; i++) {
+    let component = []
+    for (let j = 0; j < coordinates.length; j++) {
+      if (coordinates[i][0] === coordinates[j][0] || coordinates[i][1] === coordinates[j][1]) {
+        component.push(coordinates[j])
+      }
+    }
+    components.push(component)
+  }
+
+  for (let i = 0; i < components.length; i++) {
+    for (let j = 0; j < components.length; j++) {
+      if (i !== j) {
+        let component1 = components[i]
+        let component2 = components[j]
+        let component1String = component1.toString()
+        let component2String = component2.toString()
+        if (component1String.includes(component2String)) {
+          components.splice(j, 1)
+        } else if (component2String.includes(component1String)) {
+          components.splice(i, 1)
+        }
+      }
+    }
+  }
+
+  return components
+
+}
+
+const getComponentesConexas = (matrix, partialComponents) => {
+
+  let diagonalNodesVisited = Array(matrix.length).fill(0)
+  let componentesConexas = []
+  let nodesInPartialComponent = []
+  
+  // get the nodes in the partial components
+  for (let i = 0; i < partialComponents.length; i++) {
+    let component = partialComponents[i]
+    for (let j = 0; j < component.length; j++) {
+      let node = component[j]
+      nodesInPartialComponent.push(node[0])
+      nodesInPartialComponent.push(node[1])
+      diagonalNodesVisited[node[0]] = 1
+      diagonalNodesVisited[node[1]] = 1
+      nodesInPartialComponent = [...new Set(nodesInPartialComponent)]
+    }
+    componentesConexas.push(nodesInPartialComponent)
+    // clear the nodesInPartialComponent array
+    nodesInPartialComponent = []
+  }
+
+  do {
+    for (let i = 0; i < diagonalNodesVisited.length; i++) {
+      if (diagonalNodesVisited[i] === 0) {
+        let component = []
+        component.push(i)
+        diagonalNodesVisited[i] = 1
+        componentesConexas.push(component)
+      }
+    }
+  } while (diagonalNodesVisited.includes(0))
+
+  return componentesConexas
   
 }
 
@@ -131,6 +212,13 @@ const main = () => {
 
   let sortedMatrixColumns = sortMatrixColumns(sortedMatrix, nodesOrder)
   console.log(sortedMatrixColumns)
+
+  let underDiagonalNodes = getUnderDiagonalNodes(sortedMatrixColumns)
+
+  let components = divideComponents(underDiagonalNodes)
+
+  let componentesConexas = getComponentesConexas(sortedMatrixColumns, components)
+  console.log("Las componentes conexas del grafo son: ", componentesConexas)
 }
 
 main()
