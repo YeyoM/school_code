6ac351afa5382f72dedcebf2c61e3e4f1cc89c24
YeyoM <yeyomoreno2003@hotmail.com>
2022-10-17 10:38:16 -0500

    mutacionFinal refactor finished

---
+++ semester_03/estadistica/Practica distribuciones discretas 17 de oct.R
@@ -0,0 +1,122 @@
+# Práctica: distribuciones de 
+#           probabilidad discretas
+# rm(list=ls())
+# 
+# Contenido:
+#:01 Distribución binomial 
+
+#:01 Distribución binomial ----------
+# X: número de éxitos en n ensayos 
+#    de Bernoulli
+# X = 0,1,...,n
+# n >= 1, es el total de ensayos    
+# p = P(E), de cada ensayo, 0 <= p <= 1.
+
+# a) Ejemplo: apostar un peso a los 
+#    números rojos en el juego de la
+#    ruleta, n = 10 y p = 18/38 = 0.4737.
+
+# Valores de los parámetros
+n<- 20 # 10 # 
+p<- 18/38
+x<- 0:n    # conjunto de valores posibles
+f.x<- choose(n=n,k=x)*p^x*(1-p)^(n-x)
+F.x<- cumsum(f.x)
+fx<- dbinom(x=x,size=n,prob=p)
+Fx<- pbinom(q=x,size=n,prob=p)
+mat<- cbind(x,f.x,fx,F.x,Fx)
+mat<- round(mat,4)
+mat
+
+moda<- which.max(fx)
+moda
+moda<- x[moda]
+moda
+
+media<- n*p
+media
+media.formula.universal<- sum(x*fx)
+media.formula.universal
+
+aux<- which(Fx>=0.5)
+mediana<- x[aux[1]]
+mediana
+
+varianza<- n*p*(1-p)
+varianza.formula.universal<- 
+  sum((x-media)^2*fx)
+varianza.formula.universal
+desviacion.estandar<- sqrt(varianza)
+
+
+x11()
+main<- "función de densidad binomial"
+xlab<- "x: número de juegos ganados"
+ylab<- "f(x): probabilidad"
+plot(x,fx,xlab=xlab,ylab=ylab,main=main,
+     type="p",pch=19,col="red")
+rug(x,col="blue")
+points(c(media,moda),c(0,0),pch=c(3,2),
+       col=c("green","orange"))
+leyenda<- c("media","moda")
+legend(x="topright",legend=leyenda,
+       pch=c(3,2),col=c("green","orange"),
+       text.col=c("green","orange"))
+
+x11()
+main<- "función de distribución binomial"
+xlab<- "x: número de juegos ganados"
+ylab<- "F(x): probabilidad acumulada"
+plot(x,Fx,xlab=xlab,ylab=ylab,main=main,
+     type="p",pch=19,col="red")
+rug(x,col="blue")
+points(c(media,moda,mediana),
+       c(0,0,0.05),pch=c(3,2,4),
+       col=c("green","orange","yellow"))
+leyenda<- c("media","moda","mediana")
+legend(x="bottomright",legend=leyenda,
+       pch=c(3,2,4),
+       col=c("green","orange","yellow"),
+       text.col=c("green","orange","yellow"))
+
+
+
+
+# ibarra, torres, ortiz, caldera, 2022-10-10
+
+
+#:02 Distribución de Poisson -----
+#    X~Po(mi)
+#    f(x) = P(X=x) = exp(-mi)mi^x/x!
+#    para x = 0,1,2,...
+
+# Ejercicio 3.105 del libro de Montgomery
+# a)
+mi<- 0.5  
+x<- 0
+f.x<- exp(-mi)*mi^x/factorial(x)
+fx<- dpois(x=x,lambda=mi)
+c(x,fx,f.x)
+
+# b)
+mi<- 5  
+x<- 0
+f.x<- exp(-mi)*mi^x/factorial(x)
+fx<- dpois(x=x,lambda=mi)
+c(x,fx,f.x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- semester_03/inteligencia_artificial/mutacion/mutacionFinal.cpp
+++ semester_03/inteligencia_artificial/mutacion/mutacionFinal.cpp
@@ -267,13 +267,35 @@ int main() {
   /* Auxiliar para contar los ceros de cada individuo y de las generaciones */
   char auxiliar = '0';
 
+  /* Variables utilizadas para seleccionar los 6 elementos a cruazar de cada generacion */
+  int seleccionados = 0;
+  int iterador = 0;
+
+  /* 6 elementos seleccionados para cruzar */
+  int indicesAleatorios[6];
+
+  /* 4 elementos que no se seleccionaron */
+  int indicesNoAleatorios[4];
+
+  /* Indices de la generacion elegidos para mutar */
+  int indicesParaMutacion[2];
+
+  /* Indice de la generacion elegido para mutar */
+  int indiceParaMutacion;
+
+  /* Rango de bits entre 10 y 40% para mutar en cada individuo */
+  int numBits;
+
+  /* Indice del bit elegido para mutar en el individuo indiceParaMutacion */
+  int indiceBit;
+
   ///////////////////////////////////////////////////////////////////// LOOP PRINCIPAL
   for (int k = 0; k < NUMERO_DE_CADENAS; k++) {
 
     /* Generando las cadenas aleatorias. (solo en generacion 1)*/
     if (k == 0) {
       for (int i = 0; i < 10; i++) {
-        cadenaAleatoria(LONGITUD_DESEADA, destino);
+        cadenaAleatoria(LONGITUD_DESEADA, destino); // Se guarda en destino
         binarios1[k][i] = destino;
         binariosCopy1[k][i] = binarios1[k][i];
         ceros[i] = countOccurrences(auxiliar, binarios1[k][i]);
@@ -281,7 +303,6 @@ int main() {
     } else {
 
       // Seleccionamos 6 elementos al azar de la generacion anterior
-      int indicesAleatorios[6];
       for (int i = 0; i < 6; i++) {
         int indiceAleatorio = getRandomIndex(indicesAleatorios, 10);
         indicesAleatorios[i] = indiceAleatorio;
@@ -299,18 +320,16 @@ int main() {
         ceros[i] = countOccurrences(auxiliar, binarios1[k][i]);
       }
 
-      // Seleccionamos 4 elementos restantes
-      int indicesNoAleatorios[4];
-      int noSeleccionados = 0;
-      int iterador2 = 0;
+      seleccionados = 0;
+      iterador = 0;
 
       // Ingresar los 4 elementos que no se seleccionaron
       for (int j = 0; j < 10; j++) {
         bool exists = existsInArray(indicesAleatorios, 10, j);
         if (!exists) {
-          indicesNoAleatorios[iterador2] = j;
-          iterador2++;
-          noSeleccionados++;
+          indicesNoAleatorios[iterador] = j;
+          iterador++;
+          seleccionados++;
         }
       }
 
@@ -327,21 +346,23 @@ int main() {
       // contamos cuantos individuos de la generacion tienen 10 ceros
       individuosCeros = getIndividuosConDiezCeros(binarios1[k], 10);
 
+      // Seleccionamos 2 elementos al azar de la generacion anterior de los ultimos 5 o que no tengan 10 ceros
       if (individuosCeros < 8) {
-        // Seleccionamos 2 elementos al azar de la generacion anterior (los ultimos 5)
-        int indicesParaMutacion[2];
-        int seleccionados = 0;
-        int iterador = 0;
-        int indiceParaMutacion;
+
+        seleccionados = 0;
+        iterador = 0;
 
         while(seleccionados < 2) {
-          // en caso de que haya mas de cinco elementos con diez ceros
-          // solo tomamos lo elementos que aun no tienen 10 ceros
+          
+          // En caso de que ya existan mas de cinco individuos con 10 ceros 
           if (individuosCeros >= 5) {
             indiceParaMutacion = aleatorioEnRango(individuosCeros, 9);
-          } else {
+          } 
+          // Caso contrario, se eligen dos de los ultimos cinco
+          else {
             indiceParaMutacion = aleatorioEnRango(5, 9);
           }
+          // Elegimos dos individuos al azar (no repetidos)
           bool exists = existsInArray(indicesParaMutacion, 2, indiceParaMutacion);
           if (!exists) {
             indicesParaMutacion[iterador] = indiceParaMutacion;
@@ -351,18 +372,24 @@ int main() {
         }
 
         // Mutamos los 2 elementos seleccionados
-        for (int i = 0; i < 10; i++) {
-          int numBits = aleatorioEnRango(1, 4);
-          if (i == indicesParaMutacion[0] || i == indicesParaMutacion[1]) {
-            for (int j = 0; j < numBits; j++) {
-              int indiceBit = aleatorioEnRango(0, 9);
-              if (binarios1[k][i][indiceBit] == '0') {
-                binarios1[k][i][indiceBit] = '1';
-                binariosCopy1[k][i][indiceBit] = '1';
-              } else {
-                binarios1[k][i][indiceBit] = '0';
-                binariosCopy1[k][i][indiceBit] = '0';
-              }
+        for (int i = 0; i < 2; i++) {
+          
+          // Seleccionamos un rango entre 10% y 40% (1 y 4)
+          numBits = aleatorioEnRango(1, 4);
+
+          // Seleccionamos un bit al azar numBits veces
+          for (int j = 0; j < numBits; j++) {
+            indiceBit = aleatorioEnRango(0, 9);
+            
+            // Si el bit es 0, lo cambiamos a 1
+            if (binarios1[k][indicesParaMutacion[i]][indiceBit] == '0') {
+              binarios1[k][indicesParaMutacion[i]][indiceBit] = '1';
+              binariosCopy1[k][indicesParaMutacion[i]][indiceBit] = '1';
+            } 
+            // Si el bit es 1, lo cambiamos a 0
+            else {
+              binarios1[k][indicesParaMutacion[i]][indiceBit] = '0';
+              binariosCopy1[k][indicesParaMutacion[i]][indiceBit] = '0';
             }
           }
         }
@@ -377,9 +404,15 @@ int main() {
     // Ordenando
     ordenarGeneracion(binarios1[k], binariosCopy1[k], ceros); 
 
+    // Imprimiendo la generacion
     mostrarGeneracion(binarios1[k], 10, k);
+
+    // Obtenemos la suma total de ceros en la generacion
     suma = sumaCerosGeneracion(binarios1[k], 10, k);
+
+    // En caso de que suma es igual a 100, se termina el programa
     if (suma == 100) return 0;
+    
     suma = 0;
 
   }
