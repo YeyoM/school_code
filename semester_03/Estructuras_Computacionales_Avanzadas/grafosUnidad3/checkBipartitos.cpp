7bdb6ed8e3d7808df5e65cee3d512e89e4afdd23
YeyoM <yeyomoreno2003@hotmail.com>
2022-10-25 08:00:40 -0500

    clase 25 de oct

---
+++ semester_03/Estructuras_Computacionales_Avanzadas/grafosUnidad3/checkBipartitos.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * @brief Comprueba si un grafo es bipartito
+ * @param matriz Matriz de adyacencia del grafo (no dirigido)
+ * @return true si el grafo es bipartito, false en caso contrario
+ * 
+ * autor Diego Emilio Moreno Sánchez @YeyoM
+ * @date 25 de octubre de 2022
+ * 
+ * Procedimiento basado en: Matemática Discreta - Grafo Bipartido. Ej.2 - Jesús Soto (https://www.youtube.com/watch?v=0Kb4r1oLMbY)
+ * 
+ * En teoría de grafos, un grafo bipartito es un grafo G=(N,E) cuyos 
+ * vértices se pueden separar en dos conjuntos disjuntos U y V.
+ * De manera que las aristas sólo pueden conectar vértices
+ * de un conjunto con vértices del otro.
+ * 
+ * La idea para verificar si un grafo es bipartito es "colorear"
+ * cada vertice ya sea de color 1 o 2. Si un vertice tiene color 1
+ * entonces todos sus vecinos deben tener color 2 y viceversa.
+ * 
+ * De forma que para verificar, cada nodo guardará la información
+ * del color que tiene. Si un nodo no tiene color, entonces se le
+ * asigna el color 1 y se le asigna el color 2 a todos sus vecinos.
+ * 
+ * Este algoritmo se repite hasta que hayamos visitado todos los nodos.
+ * 
+*/
+
+#define NODOS 6  
+
+struct Nodo {
+  int color;
+  int id;
+};
+
+int main() {
+
+  int colorActual = 0; 
+
+  // Matriz de adyacencia del grafo
+  int matriz[NODOS][NODOS] = {
+    {0, 0, 1, 1, 0, 1},
+    {0, 0, 0, 0, 1, 1},
+    {1, 0, 0, 0, 0, 0},
+    {1, 0, 0, 0, 0, 0},
+    {0, 1, 0, 0, 0, 0},
+    {1, 1, 0, 0, 0, 0}
+  };
+
+  // Arreglo de nodos
+  Nodo nodos[NODOS];
+  for (int i = 0; i < NODOS; i++) {
+    nodos[i].id = i;
+    nodos[i].color = 0;
+  }
+
+  // Colorear el primer nodo
+  nodos[0].color = 1;
+
+  for (int i = 0; i < NODOS; i++) {
+    for (int j = 0; j < NODOS; j++) {
+      
+      colorActual = nodos[i].color;
+      
+      if (matriz[i][j] == 1) {
+        if (nodos[j].color == 0) {
+          nodos[j].color = colorActual == 1 ? 2 : 1;
+        } else if (nodos[j].color == colorActual) {
+          cout << "El grafo no es bipartito" << endl;
+          return 0;
+        }
+      }
+    }
+  }
+
+  cout << "El grafo es bipartito" << endl;
+
+  return 0;
+}
