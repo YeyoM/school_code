3dc2710a169c0c2485dee247daaa6619d0df8bff
YeyoM <yeyomoreno2003@hotmail.com>
2022-11-23 18:39:56 -0600

    iniciando nuevo proyecto

---
+++ semester_03/Lenguajes de Computacion 3/miniMaple/src/minimaple/MiniMaple.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package minimaple;
+import org.apache.commons.lang3.math.Fraction;
+
+/**
+ *
+ * @author Usuario
+ */
+public class MiniMaple {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(Fraction.getFraction(1, 4));
+    }
+    
+}
