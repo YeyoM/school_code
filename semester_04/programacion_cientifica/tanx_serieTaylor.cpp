0447af2da4cb0025ad6ac55b50204fe860440d2b
YeyoM <yeyomoreno2003@hotmail.com>
2023-02-14 10:54:31 -0600

    Funcion tan x con series de taylor

---
+++ semester_04/programacion_cientifica/tanx_serieTaylor.cpp
@@ -0,0 +1,23 @@
+// Universidad Autonoma de Aguascalientes
+// Centro de Ciencias Basicas
+// Departamento de Ciencaas de la Computacion
+// Programacion Cientifica	
+// Diego Emilio Moreno Sánchez
+// Profesor:
+
+// Programa que calcula el valor de tan(x) 
+// para un valor de x, utilizando series de Taylor
+
+// Entradas: x (grados), n (número de términos de la serie de Taylor)
+// nota - el número de términos debe ser 2, 4 o 6
+
+// Salidas: valor de tan(x) y valor de tan(x) calculado 
+// con la función tan(x) (cmath)
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  return 0;
+}
