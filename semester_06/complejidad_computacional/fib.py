fa444d802a5a11bbead60b44b5e9c9ddd3a18d26
YeyoM <yeyomoreno2003@hotmail.com>
2024-02-09 17:06:24 -0600

    feat: :sparkles: fib

--- semester_06/complejidad_computacional/fib.py
+++ semester_06/complejidad_computacional/fib.py
@@ -1,18 +1,24 @@
-# func que imprime los numeros del 1 al 10
-def func(n):
-    print(n)
-    if n < 10:
-        func(n + 1)
+# Funcion que calcula el n-esimo numero de la serie de Fibonacci
+# En este caso intentaremos calcular hasta el termino 21
+# de manera recursiva, y calcularemos la complejidad
 
 
-# func que imprime los numeros del 10 al 1
-def func_(n):
-    if n < 10:
-        func_(n + 1)
-    print(n)
+def fib(n):
+    if n <= 1:
+        return n
+    return fib(n - 1) + fib(n - 2)
 
 
-print("Primero: ")
-func(1)
-print("Segundo: ")
-func_(1)
+# Calculamos la complejidad
+
+"""
+La razón de esta complejidad es que, en cada llamada recursiva,
+la función se divide en dos subproblemas, y cada uno de esos 
+subproblemas a su vez se divide en dos subproblemas, y así 
+sucesivamente. Esto resulta en una cantidad exponencial de 
+llamadas a la función, lo que hace que el tiempo de ejecución 
+crezca rápidamente a medida que aumenta el valor de "n".
+"""
+
+# Probamos la función
+print(fib(21))
